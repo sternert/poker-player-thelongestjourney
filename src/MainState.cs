62382210f683c351eab531f19d29e8a890056a06
Tor Sterner <tor.sterner@svea.com>
2016-11-29 09:29:44 +0100

    First

---
+++ src/MainState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Nancy.Simple
+{
+    public class HoleCard
+    {
+        public string rank { get; set; }
+        public string suit { get; set; }
+    }
+
+    public class Player
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string version { get; set; }
+        public int stack { get; set; }
+        public int bet { get; set; }
+        public List<HoleCard> hole_cards { get; set; }
+    }
+
+    public class CommunityCard
+    {
+        public string rank { get; set; }
+        public string suit { get; set; }
+    }
+
+    public class MainState
+    {
+        public string tournament_id { get; set; }
+        public string game_id { get; set; }
+        public int round { get; set; }
+        public int bet_index { get; set; }
+        public int small_blind { get; set; }
+        public int current_buy_in { get; set; }
+        public int pot { get; set; }
+        public int minimum_raise { get; set; }
+        public int dealer { get; set; }
+        public int orbits { get; set; }
+        public int in_action { get; set; }
+        public List<Player> players { get; set; }
+        public List<CommunityCard> community_cards { get; set; }
+    }
+}
--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -4,12 +4,15 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+		public static readonly string VERSION = "1.0.0";
 
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			return 0;
+            //TODO: Use this method to return the value You want to bet
+            var state = gameState.ToObject<MainState>();
+		    var player = state.players[state.in_action];
+
+            return state.current_buy_in - player.bet + state.minimum_raise;
 		}
 
 		public static void ShowDown(JObject gameState)
