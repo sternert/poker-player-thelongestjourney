04b80772a742f56cce159f8b7aac82a9ad77a5f5
chaudi <cederfelt@live.com>
2016-11-29 09:59:51 +0100

    smarter

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -2,32 +2,89 @@
 
 namespace Nancy.Simple
 {
-	public static class PokerPlayer
-	{
-		public static readonly string VERSION = "1.0.0";
+    public static class PokerPlayer
+    {
+        public static readonly string VERSION = "1.0.0";
 
-		public static int BetRequest(JObject gameState)
-		{
+        public static int BetRequest(JObject gameState)
+        {
             //TODO: Use this method to return the value You want to bet
             var state = gameState.ToObject<MainState>();
-		    var player = state.players[state.in_action];
+            var player = state.players[state.in_action];
 
 
-		    var card1 = player.hole_cards[0];
+            var card1 = player.hole_cards[0];
             var card2 = player.hole_cards[1];
 
-		    if (card1.rank == card2.rank)
-		    {
+            if (highcard(card1, card2))
+            {
+                return state.current_buy_in - player.bet + state.minimum_raise;
+            }
+
+            if (card1.rank == card2.rank)
+            {
                 return state.current_buy_in - player.bet + state.minimum_raise * 4;
             }
 
             return state.current_buy_in - player.bet + state.minimum_raise;
-		}
+        }
 
-		public static void ShowDown(JObject gameState)
-		{
-			//TODO: Use this method to showdown
-		}
-	}
+        private static int CardPoints(HoleCard card)
+        {
+            if (card.rank == "A")
+            {
+                return 100;
+            }
+
+            if (card.rank == "K")
+            {
+                return 60;
+            }
+
+            if (card.rank == "Q")
+            {
+                return 40;
+            }
+
+            if (card.rank == "J")
+            {
+                return 20;
+            }
+
+            if (card.rank == "10")
+            {
+                return 10;
+            }
+
+            return 0;
+        }
+
+        private static bool highcard(HoleCard card1, HoleCard card2)
+        {
+            int points = 0;
+
+
+            points += CardPoints(card1);
+            points += CardPoints(card2);
+
+            if (card1.suit == card2.suit)
+            {
+                points += 50;
+            }
+
+            if (points > 100)
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+
+        public static void ShowDown(JObject gameState)
+        {
+            //TODO: Use this method to showdown
+        }
+    }
 }
 
